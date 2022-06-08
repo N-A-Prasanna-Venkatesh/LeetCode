1b2cc83687d032ed0dcd81370606f18e6f3a58c6
N A Prasanna Venkatesh <52624854+N-A-Prasanna-Venkatesh@users.noreply.github.com>
2022-06-08 21:07:19 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int removePalindromeSub(string s) {
         if(s=="") return 0;
-        int i=0,j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j]){
-                return 2;
-            }
-            i++;
-            j--;
-        }
-        return 1;
+        string s2=s;
+        reverse(s.begin(),s.end());
+        if(s==s2) return 1;
+        return 2;
     }
 };
