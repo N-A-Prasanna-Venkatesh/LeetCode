b2938d1b1a68ec3a6044fdd1c5fda6853db21165
N A Prasanna Venkatesh <52624854+N-A-Prasanna-Venkatesh@users.noreply.github.com>
2022-07-06 19:16:06 +0530

    Time: 6 ms (41.45%), Space: 6.1 MB (18.24%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        vector<int>fib;
+        fib.push_back(0);
+        fib.push_back(1);
+        if(n<=1)
+            return fib[n];
+        else
+            for(int i=2;i<=n;i++)
+                fib.push_back(fib[i-1]+fib[i-2]);
+        
+        return fib[n];
+    }
+};
