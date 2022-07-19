6dfbeb1e3de582e1b4275a31e3664dae6910b888
N A Prasanna Venkatesh <52624854+N-A-Prasanna-Venkatesh@users.noreply.github.com>
2022-07-19 10:19:51 +0530

    Time: 63 ms (28.82%), Space: 13.4 MB (40.74%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -5,10 +5,9 @@ public:
         int len = c.length();
         if(len<=2) return false;
         for(int i=1;i<len-1;i++)
-            if((c[i-1] == c[i]) && (c[i] == c[i+1])){
+            if(c[i-1] == c[i] && c[i] == c[i+1]){
                 chances[c[i]]++;
             }
-        if(chances['A'] > chances['B']) return true;
-        return false;
+        return chances['A'] > chances['B'];
     }
 };
